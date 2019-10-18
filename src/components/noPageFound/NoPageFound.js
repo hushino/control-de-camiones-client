9b887b66379e5da8c1e646b1662362378801b263
hushino <frostshock@outlook.com>
2019-10-18 15:09:45 -0300

    css ant desing

---
+++ src/components/noPageFound/NoPageFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NoPageFound() {
+    return (
+        <div><h1>NoPageFound</h1></div>
+    )
+}
+
+export default NoPageFound
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Provider } from 'react-redux'
+import * as stores from './redux/store'
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Header from './components/header/Header';
+
+ReactDOM.render(
+    <Provider store={stores.default}>
+        <Header />
+    </Provider>,
+    document.getElementById('header'));
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
